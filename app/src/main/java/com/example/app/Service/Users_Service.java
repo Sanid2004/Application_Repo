9155c0f48e89aebdd147757c6ed01fe9182037d7
Sanid2004 <22ucs182@lnmiit.ac.in>
2025-05-30 10:25:11 +0530

    Update Users_Service.java

--- app/src/main/java/com/example/app/Service/Users_Service.java
+++ app/src/main/java/com/example/app/Service/Users_Service.java
@@ -4,7 +4,7 @@ package com.example.app.Service;
 import com.example.app.Entities.User;
 
 public interface Users_Service{
-    public String get_query();
+    public String get_query(Final_Query final_query);
 
     public User add_user(User user);
 }
